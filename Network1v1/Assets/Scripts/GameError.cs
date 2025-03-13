c6e46ccbe474bc06003d3147948295617af7ef4e
Soph <sophearle16@gmail.com>
2025-03-13 15:09:53 +0000

    did things

---
+++ Network1v1/Assets/Scripts/GameError.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameError : MonoBehaviour
+{
+    public void CloseGameError()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Network1v1/Assets/Scripts/NetworkManagerController.cs
+++ Network1v1/Assets/Scripts/NetworkManagerController.cs
@@ -7,19 +7,57 @@ public class NetworkManagerController : MonoBehaviour
 {
     public NetworkManager networkManager;
 
+
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject AlreadyHostingError;
+    [SerializeField] private GameObject GameFullError;
+
+    private int numOfPlayers;
+
+    private void Awake()
+    {
+        numOfPlayers = 0;
+        MainMenu.SetActive(true);
+
+        GameFullError.SetActive(false);
+    }
+
+
     private void Start()
     {
         networkManager.OnClientConnectedCallback += OnClientConnect;
 
-#if UNITY_EDITOR
         networkManager.StartHost();
-#else
-        networkManager.StartClient();
-#endif
     }
 
     private void OnClientConnect(ulong obj)
     {
         Debug.Log("Client Connected");
+    }
+
+
+    private void EnterGame()
+    {
+        numOfPlayers++;
+        MainMenu.SetActive(false);
+    }
+
+    public void EnterAsHost()
+    {
+        EnterGame();
+        networkManager.StartHost();
+    }
+
+    public void EnterAsClient()
+    {
+        if (numOfPlayers < 2)
+        {
+            EnterGame();
+            networkManager.StartClient();
+        }
+        else
+        {
+            GameFullError.SetActive(true);
+        }
     }
 }
